84f60f9e5ed815bd71bfa4459b641ea48b3e420e
JaviEPN <alex_javi_boy94@hotmail.com>
2017-08-02 17:41:30 -0500

    Codigo Proyecto Alex Pinaida

---
+++ Test/app.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    selected: 'A',
+    options: [
+      { text: 'Desarrollo', value: 'Desarrollo' },
+      { text: 'Investigacion', value: 'Investigacion' },
+      { text: 'Diseño', value: 'Diseño' }
+    ]
+  }
+})
